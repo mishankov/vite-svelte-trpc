528d32d7d3f7f7ad17858311f8aeecd8b95e5c67
Denis Mishankov <mishankov@mail.com>
2022-12-27 18:06:31 +0300

    Tests (#4)
    * add tests

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  webServer: {
+    command: 'npm run build && npm run start',
+    port: 5000
+  }
+};
+
+export default config;
