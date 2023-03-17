c30da39c24d2b10ffad05b24b2991f22a6306166
mishankov <denis@mishankov.dev>
2023-03-17 16:28:50 +0300

    fix client build folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/trpc': 'http://localhost:5000/'
-    }
+      "/trpc": "http://localhost:5000/",
+    },
   },
-  build: {outDir: 'dist/client'},
+  build: { outDir: "dist/public" },
   plugins: [svelte()],
-})
+});
