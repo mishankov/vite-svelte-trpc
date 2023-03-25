b524c65e77f237eb9c89f52feaf80d4cb3673196
Denis Mishankov <mishankov@mail.com>
2023-03-25 11:20:48 +0300

    fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'npm run build && npm run start',
-    port: 5000
-  }
+    command: "npm run build && npm run start",
+    port: 5000,
+  },
+  testDir: "e2e",
 };
 
 export default config;
