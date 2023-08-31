88e95fe25e112c9e2f833a47cc9043b8572ae41f
Denis Mishankov <mishankov@mail.com>
2023-08-31 18:59:19 +0300

    remove unnecessary sveltekit ad

--- src/App.svelte
+++ src/App.svelte
@@ -36,10 +36,6 @@
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
   <p class="read-the-docs">
     Click on the Vite, Svelte and tRPC logos to learn more
   </p>
